4dfd95c0bd7250de503873a1fab3260e6494d2bd
MzKyle <153496289+MzKyle@users.noreply.github.com>
2025-10-15 11:14:21 +0800

    ob获取内参与畸变系数
    奥比中光深度相机基于SDK获取内参与畸变系数

--- ob_Internal_acquisition.cpp
+++ ob_Internal_acquisition.cpp
@@ -20,12 +20,27 @@ int main() {
     }
 
     OBCameraIntrinsic depthIntrinsic = depthProfile->getIntrinsic();
+    OBCameraDistortion depthDistortion = depthProfile->getDistortion();
+
+
+    /*typedef struct {
+        float k1;  ///< Radial distortion factor 1
+        float k2;  ///< Radial distortion factor 2
+        float k3;  ///< Radial distortion factor 3
+        float k4;  ///< Radial distortion factor 4
+        float k5;  ///< Radial distortion factor 5
+        float k6;  ///< Radial distortion factor 6
+        float p1;  ///< Tangential distortion factor 1
+        float p2;  ///< Tangential distortion factor 2
+    } OBCameraDistortion, ob_camera_distortion;*/
 
     if (depthIntrinsic.width > 0 && depthIntrinsic.height > 0) {
         std::cout << "深度相机内参：" << std::endl;
         std::cout << "分辨率：" << depthIntrinsic.width << "x" << depthIntrinsic.height << std::endl;
         std::cout << "焦距(fx, fy)：" << depthIntrinsic.fx << ", " << depthIntrinsic.fy << std::endl;
         std::cout << "主点(cx, cy)：" << depthIntrinsic.cx << ", " << depthIntrinsic.cy << std::endl;
+        std::cout << "径向畸变系数k(1,2,3,4,5,6): " << depthDistortion.k1 << ", " << depthDistortion.k2 << ", " << depthDistortion.k3 << ", " << depthDistortion.k4 << ", " << depthDistortion.k5 << ", " << depthDistortion.k6 << std::endl;
+        std::cout << "切向畸变系数p（1，2）：" << depthDistortion.p1 << ", " << depthDistortion.p2 << std::endl;
     }
     else {
         std::cerr << "获取深度相机内参失败！" << std::endl;
